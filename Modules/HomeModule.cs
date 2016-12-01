2b6097c2f0778f0f9d631ff7d01b730a388d0fbf
Levi Bibo and Taylor Loftis-Kim <levi+taylor@>
2016-12-01 13:18:51 -0800

    Make proper output in index.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,7 +10,16 @@ namespace Weekday
 		{
 			Get["/"] = _ =>
 			{
-				return View["index.cshtml"];
+				List<string> Model = new List<string> {};
+				Model.Add("false");
+				return View["index.cshtml", Model];
+			};
+			Post["/"] = _ =>
+			{
+				List<string> Model = new List<string> {};
+				Model.Add("true");
+				Model.Add(Day.GetDayOfWeek(Request.Form["Year"],Request.Form["Month"],Request.Form["Day"]));
+				return View["index.cshtml", Model];
 			};
 		}
 	}
